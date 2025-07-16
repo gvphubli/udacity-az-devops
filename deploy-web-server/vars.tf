8a1985af4a78ca0bba58f7e4a94c67db3eef5b52
TechnologyYogi <15898156+gvphubli@users.noreply.github.com>
2025-07-16 08:32:46 -0700

    Update vars.tf

--- deploy-web-server/vars.tf
+++ deploy-web-server/vars.tf
@@ -13,24 +13,24 @@ variable "location" {
   default = "eastus" 
 }
 
-variable "subscription_id" {  
+variable "subscription_id" {
     description = "Azure subscription id"
-    default = "fbf787c1-ed83-44ad-946e-6ff3b027c400"
+    default = "8b8fdec5-790a-4029-a7e5-a8b5cb5f4e50"
 }
 
 variable "tenant_id" {
     description = "Tenant ID"
-    default = "f958e84a-92b8-439f-a62d-4f45996b6d07"
+    default = "93433ec6-28a5-4756-b39c-baa0d6174e40"
 }
 
-variable "client_id" {  
+variable "client_id" {
     description = "Client ID"
-    default = "23104103-154d-4a96-bc54-1dc16b91cf08"
+    default = "e0a8e7fe-3905-4016-a5c7-dc101a4139f6"
 }
 
-variable "client_secret" {  
+variable "client_secret" {
     description = "Client secret id"
-    default = "h.n8Q~tF7snvBI4yvjw8S~qu_1OptXjeyEm6QbpC"
+    default = "lxO8Q~uHjkprQZiOhvGKSlmGf14smUZuwZbuxcYX"
 }
 
 variable "vm_count" {
